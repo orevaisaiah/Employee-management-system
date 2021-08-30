3aac8a8983ba9f1748be44d7f0f9477941d6b283
orevaisaiah <85342927+orevaisaiah@users.noreply.github.com>
2021-08-30 13:01:30 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import LandingPage from "./components/landingpage/LandingPage";
-import Signup from "./components/landingpage/Signup";
-import Login from "./components/landingpage/Login";
+import CompLandingPage from "./components/CompLandingPage";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Route exact path="/" component={LandingPage} />
-        <div style={{ width: "100%" }}>
-          <Route exact path="/signup" component={Signup} />
-          <Route exact path="/login" component={Login} />
-        </div>
-      </div>
-    </Router>
+    <div className="App">
+      <CompLandingPage />
+    </div>
   );
 }
 
